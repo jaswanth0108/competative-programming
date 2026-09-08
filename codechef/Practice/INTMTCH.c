18014922e0348b894332168f7f9328394d609fe8
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-08 14:27:42 +0530

    Solved: INTMTCH (codechef)

---
+++ codechef/Practice/INTMTCH.c
@@ -0,0 +1,8 @@
+// Problem: INTMTCH
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/INTMTCH
+// Solved on: 2026-09-08T08:57:40.718Z
+
+// source not captured automatically - open the solution page and copy it manually
