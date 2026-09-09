f84694200a0f2eefc37a1840da115219d2608ebb
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-09 20:57:19 +0530

    Solved: UNQSHF (codechef)

---
+++ codechef/Practice/UNQSHF.c
@@ -0,0 +1,20 @@
+// Problem: UNQSHF
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255D/problems/UNQSHF
+// Solved on: 2026-09-09T15:27:19.260Z
+
+4
+2
+ab
+aa
+3
+abb
+baa
+4
+aabb
+baba
+4
+abba
+aaab
