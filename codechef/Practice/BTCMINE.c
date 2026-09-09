056471b9fde43b86eaca6ac77625b5edff62559a
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-09 21:40:40 +0530

    Solved: BTCMINE (codechef)

---
+++ codechef/Practice/BTCMINE.c
@@ -0,0 +1,8 @@
+// Problem: BTCMINE
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255D/problems/BTCMINE
+// Solved on: 2026-09-09T16:10:39.709Z
+
+// source not captured automatically - open the solution page and copy it manually
