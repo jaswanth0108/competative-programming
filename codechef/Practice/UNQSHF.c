d29599936db6dca77babe95823ccb92b1f3b4b73
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-09 21:15:12 +0530

    Solved: UNQSHF (codechef)

--- codechef/Practice/UNQSHF.c
+++ codechef/Practice/UNQSHF.c
@@ -3,6 +3,6 @@
 // Language: C​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START255D/problems/UNQSHF
-// Solved on: 2026-09-09T15:41:53.478Z
+// Solved on: 2026-09-09T15:45:11.033Z
 
 // source not captured automatically - open the solution page and copy it manually
