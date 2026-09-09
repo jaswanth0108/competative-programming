c60a7f97cc01432bab533887acfbadd15ed6e84d
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-09 20:57:14 +0530

    Solved: EGGBUY (codechef)

--- codechef/Practice/EGGBUY.c
+++ codechef/Practice/EGGBUY.c
@@ -3,6 +3,6 @@
 // Language: C​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START255D/problems/EGGBUY
-// Solved on: 2026-09-09T15:23:31.092Z
+// Solved on: 2026-09-09T15:27:14.218Z
 
 // source not captured automatically - open the solution page and copy it manually
