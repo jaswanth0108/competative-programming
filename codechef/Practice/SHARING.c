caeb3a6e59192e005f24853d575041d433821608
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-16 21:26:53 +0530

    Solved: SHARING (codechef)

--- codechef/Practice/SHARING.c
+++ codechef/Practice/SHARING.c
@@ -3,6 +3,6 @@
 // Language: C​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START256D/problems/SHARING
-// Solved on: 2026-09-16T15:00:00.663Z
+// Solved on: 2026-09-16T15:56:52.794Z
 
-6 2
+// source not captured automatically - open the solution page and copy it manually
