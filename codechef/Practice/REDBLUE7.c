2d1d63a9720040bf44bc799d403facca70545f88
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-16 21:46:58 +0530

    Solved: REDBLUE7 (codechef)

---
+++ codechef/Practice/REDBLUE7.c
@@ -0,0 +1,14 @@
+// Problem: REDBLUE7
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START256D/problems/REDBLUE7
+// Solved on: 2026-09-16T16:16:57.201Z
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+}
+
