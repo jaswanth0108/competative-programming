a04adb232fd9bf48e241aa6b45fc874e0ffb8c46
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-16 21:36:53 +0530

    Solved: FIXEDPTS (codechef)

---
+++ codechef/Practice/FIXEDPTS.c
@@ -0,0 +1,14 @@
+// Problem: FIXEDPTS
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START256D/problems/FIXEDPTS
+// Solved on: 2026-09-16T16:06:52.197Z
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+}
+
