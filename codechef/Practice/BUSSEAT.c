8823b8deaeec765fd3201ee3f7d8d8fd9cc6a6a6
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-16 21:27:26 +0530

    Solved: BUSSEAT (codechef)

---
+++ codechef/Practice/BUSSEAT.c
@@ -0,0 +1,11 @@
+// Problem: BUSSEAT
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START256D/problems/BUSSEAT
+// Solved on: 2026-09-16T15:57:25.117Z
+
+3
+2 3
+2 1
+4 8
