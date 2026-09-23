6fc24f22259ed568eb521d14085d751ff1729ce2
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-23 20:54:37 +0530

    Solved: LITUP (codechef)

---
+++ codechef/Practice/LITUP.c
@@ -0,0 +1,14 @@
+// Problem: LITUP
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/LITUP
+// Solved on: 2026-09-23T15:24:36.852Z
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+}
+
