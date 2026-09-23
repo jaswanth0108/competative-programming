1fe56ce7bc7d3f7f6627909f678b51080a41b90b
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-23 21:42:29 +0530

    Solved: FALLPR (codechef)

---
+++ codechef/Practice/FALLPR.c
@@ -0,0 +1,16 @@
+// Problem: FALLPR
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/FALLPR
+// Solved on: 2026-09-23T16:12:28.974Z
+
+4
+4
+1 0 2 -1
+3
+1 -2 -2
+5
+2 -1 -2 1 0
+4
+0 -1 -1 3
