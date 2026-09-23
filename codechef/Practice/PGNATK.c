e808738006192ecdad27c8c36f3dd96aeb10e073
Jaswanth <jaswanthnanepalli@gmail.com>
2026-09-23 20:44:49 +0530

    Solved: PGNATK (codechef)

---
+++ codechef/Practice/PGNATK.c
@@ -0,0 +1,14 @@
+// Problem: PGNATK
+// Platform: codechef
+// Language: C​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/PGNATK
+// Solved on: 2026-09-23T15:14:48.251Z
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+}
+
